ca1ed0e7d15d35f519b1c59c75e8d46d898a3c1d
Ben Kotze <ben.kotze@entelect.co.za>
2020-11-21 16:13:54 +0200

    Fixed slide implementation

--- src/components/FaqCarousel.tsx
+++ src/components/FaqCarousel.tsx
@@ -11,97 +11,73 @@ import Slide from './Slide';
 const FaqCarousel: React.FC = () => {
   return (
     <Carousel>
-      <Carousel.Item style={{ backgroundImage: 'url(' + question_mark + ')' }}>
-        <Carousel.Caption>
-          <div className="bg-masked-dark p-2 rounded-xl">
-            <h1 className="montserrat">
-              <b>What</b> is Street Epistemology?
-            </h1>
-            <h4>
-              While definitions vary, it's generally accepted that Street
-              Epistemology is a conversational tool that helps people reflect on
-              the quality of their reasons and the reliability of their methods
-              used to derive one's confidence level in their deeply-held
-              beliefs.
-            </h4>
-          </div>
-        </Carousel.Caption>
+      <Carousel.Item>
+        <Slide
+          title="What is Street Epistemology?"
+          description="
+                  While definitions vary, it's generally accepted that Street
+                  Epistemology is a conversational tool that helps people
+                  reflect on the quality of their reasons and the reliability of
+                  their methods used to derive one's confidence level in their
+                  deeply-held beliefs."
+          background={question_mark}
+        />
       </Carousel.Item>
       <Carousel.Item style={{ backgroundImage: 'url(' + light_bulb + ')' }}>
-        <Carousel.Caption>
-          <div className="bg-masked-dark p-2 rounded-xl">
-            <h1 className="montserrat">
-              <b>Why</b> use Street Epistemology?
-            </h1>
-            <h4>
-              Although practitioners' objecives will differ, Street Epistemology
-              is generally used to understand a claim, identify the actual
-              reasons and reliability of the method supporting the claim, and
-              see if one's confidence in their claim is justified.
-            </h4>
-          </div>
-        </Carousel.Caption>
+        <Slide
+          title="Why use Street Epistemology?"
+          description="
+          Although practitioners' objecives will differ, Street Epistemology
+          is generally used to understand a claim, identify the actual
+          reasons and reliability of the method supporting the claim, and
+          see if one's confidence in their claim is justified."
+          background={light_bulb}
+        />
       </Carousel.Item>
       <Carousel.Item
         style={{ backgroundImage: 'url(' + buildings_aerial_view + ')' }}
       >
-        <Carousel.Caption>
-          <div className="bg-masked-dark p-2 rounded-xl">
-            <h1 className="montserrat">
-              <b>Where</b> to use Street Epistemology?
-            </h1>
-            <h4>
-              While every venue has pros and cons, Street Epistemology can be
-              practiced virtually anywhere, including face-to-face, video chat,
-              audio chat, text chat, and social media, just to name a few.
-            </h4>
-          </div>
-        </Carousel.Caption>
+        <Slide
+          title="Where to use Street Epistemology?"
+          description="
+        While every venue has pros and cons, Street Epistemology can be
+        practiced virtually anywhere, including face-to-face, video chat,
+        audio chat, text chat, and social media, just to name a few."
+          background={buildings_aerial_view}
+        />
       </Carousel.Item>
       <Carousel.Item style={{ backgroundImage: 'url(' + pocket_watch + ')' }}>
-        <Carousel.Caption>
-          <div className="bg-masked-dark p-2 rounded-xl">
-            <h1 className="montserrat">
-              <b>When</b> to use Street Epistemology?
-            </h1>
-            <h4>
-              If you have willing participants and applied good judgment of the
-              situation, it's probably a great time to use Street Epistemology
-              on someone's claim with them.
-            </h4>
-          </div>
-        </Carousel.Caption>
+        <Slide
+          title="When to use Street Epistemology?"
+          description="
+          If you have willing participants and applied good judgment of the
+          situation, it's probably a great time to use Street Epistemology
+          on someone's claim with them."
+          background={pocket_watch}
+        />
       </Carousel.Item>
       <Carousel.Item
         style={{ backgroundImage: 'url(' + pedestrian_crossing + ')' }}
       >
-        <Carousel.Caption>
-          <div className="bg-masked-dark p-2 rounded-xl">
-            <h1 className="montserrat">
-              <b>Who</b> is Street Epistemology for?
-            </h1>
-            <h4>
-              While this method originated in the atheist community, we think
-              everyone should learn Street Epistemology, regardless of where
-              someone happens to stand on any claim.
-            </h4>
-          </div>
-        </Carousel.Caption>
+        <Slide
+          title="Who is Street Epistemology for?"
+          description="
+        While this method originated in the atheist community, we think
+        everyone should learn Street Epistemology, regardless of where
+        someone happens to stand on any claim."
+          background={pedestrian_crossing}
+        />
       </Carousel.Item>
       <Carousel.Item style={{ backgroundImage: 'url(' + hand_shake + ')' }}>
-        <Carousel.Caption>
-          <div className="bg-masked-dark p-2 rounded-xl">
-            <h1 className="montserrat">
-              <b>How</b> to use Street Epistemology?
-            </h1>
-            <h4>
-              This website is your entry point to all of the resources and
-              communities currently available to help you learn, practice,
-              conduct, and improve Street Epistemology, and we're very happy to
-              have you here.
-            </h4>
-          </div>
-        </Carousel.Caption>
+        <Slide
+          title="How to use Street Epistemology?"
+          description="
+          This website is your entry point to all of the resources and
+          communities currently available to help you learn, practice,
+          conduct, and improve Street Epistemology, and we're very happy to
+          have you here."
+          background={hand_shake}
+        />
       </Carousel.Item>
     </Carousel>
   );
--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
+import { Container, Row } from 'react-bootstrap';
 
 export interface SlideProps {
   background: string;
@@ -7,16 +7,26 @@ export interface SlideProps {
   description: string;
 }
 
-const Slide: React.FC<SlideProps> = ({ background, title, description }) => {
+const Slide: React.FC<SlideProps> = ({
+  background,
+  title,
+  description,
+}: SlideProps) => {
   return (
-    <Carousel.Item style={{ backgroundImage: background }}>
-      <Carousel.Caption>
-        <div className="bg-masked-dark p-2 rounded-xl">
-          <h1 className="montserrat">{title}</h1>
-          <h4>{description}</h4>
-        </div>
-      </Carousel.Caption>
-    </Carousel.Item>
+    <Container
+      fluid
+      className="d-flex align-items-center carousel-slide text-light text-center"
+      style={{ backgroundImage: 'url(' + background + ')' }}
+    >
+      <Container>
+        <Row>
+          <div className="bg-masked-dark p-2 rounded-xl">
+            <h1 className="montserrat">{title}</h1>
+            <h4>{description}</h4>
+          </div>
+        </Row>
+      </Container>
+    </Container>
   );
 };
 
