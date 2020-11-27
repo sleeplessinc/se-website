6bdc1f010145a7c2af3541146d1f01f92fc4344b
Ben Kotze <befkotze@gmail.com>
2020-11-27 18:41:26 +0200

    Firebase API config and get collections

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -1,14 +1,17 @@
 import app from 'firebase/app';
 import 'firebase/database';
+import CollectionType from '../enums/CollectionType';
+import CardDetails from '../models/CardDetails';
+import * as mapper from '../utils/mapper';
 
 const config = {
-  apiKey: '',
-  authDomain: 'se-survey.firebaseapp.com',
-  databaseURL: 'https://se-survey.firebaseio.com',
-  projectId: 'se-survey',
-  storageBucket: 'se-survey.appspot.com',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: 'AIzaSyBBmfx-gBbK-zr2S2aVN5cJeTXo1BrX1OI',
+  authDomain: 'se-website-fe4a4.firebaseapp.com',
+  databaseURL: 'https://se-website-fe4a4.firebaseio.com',
+  projectId: 'se-website-fe4a4',
+  storageBucket: 'se-website-fe4a4.appspot.com',
+  messagingSenderId: '46383537676',
+  appId: '1:46383537676:web:9dacd993f857c3cec1d829',
 };
 
 class Firebase {
@@ -17,6 +20,28 @@ class Firebase {
   }
   database() {
     return app.database();
+  }
+
+  subscribeToCollection(
+    collectionType: CollectionType,
+    callback: (sessions: CardDetails[]) => void,
+    cancelCallbackOrContext?: (error: any) => void,
+  ): void {
+    app
+      .database()
+      .ref(CollectionType[collectionType].toLowerCase())
+      .on(
+        'value',
+        (snapshot) => {
+          const val = snapshot.val();
+          const sessions = Object.keys(val).map((key) => {
+            const session = mapper.mapAnyToCardDetails(val[key]);
+            return session;
+          });
+          callback(sessions);
+        },
+        cancelCallbackOrContext,
+      );
   }
 }
 
