fb391a7e2e8d4ff43c9037a179b76e3d6a4db5de
Ben Kotze <befkotze@gmail.com>
2020-12-02 11:58:49 +0200

    Add footer to app

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import bg_question_mark from './images/bg_question_mark.webp';
 import BlogListPage from './components/BlogListPage';
 import BlogPage from './components/BlogPage';
 import PageNotFound from './components/PageNotFound';
+import Footer from './components/Footer';
 
 const App: React.FC = () => {
   return (
@@ -37,6 +38,7 @@ const App: React.FC = () => {
             <PageNotFound />
           </Route>
         </Switch>
+        <Footer />
       </div>
     </Router>
   );
