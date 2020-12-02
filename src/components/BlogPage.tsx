42e89c458a8018e52df35b340c4d6743b035264d
Ben Kotze <befkotze@gmail.com>
2020-12-02 09:34:13 +0200

    Display 404 when an invalid blog name is requested

--- src/components/BlogPage.tsx
+++ src/components/BlogPage.tsx
@@ -1,28 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/esm/Container';
 import { FirebaseContext } from '../firebase';
 import parse from 'html-react-parser';
 import { useParams } from 'react-router-dom';
 import useStateWithLocalStorage from '../utils/storage';
+import PageNotFound from './PageNotFound';
 
 const BlogPage: React.FC = () => {
   const firebaseContext = React.useContext(FirebaseContext);
   const { id } = useParams();
   const path = `blog/${id}`;
   const [content, setContent] = useStateWithLocalStorage(path);
+  const [notFound, setnotFound] = useState(false);
   useEffect(() => {
     firebaseContext?.subscribeToPage(
       path,
       (results) => {
-        setContent(results);
+        if (!results || results === '') {
+          setnotFound(true);
+        } else {
+          setContent(results);
+        }
       },
       (error) => {
+        setnotFound(true);
         console.log(error);
       },
     );
   }, [firebaseContext]);
-  console.log('Content: ' + content);
-  return <Container>{parse(content)}</Container>;
+  console.log(content);
+  return notFound ? <PageNotFound /> : <Container>{parse(content)}</Container>;
 };
 
 export default BlogPage;
