2d0a35b28083ef11210908b266a345f4cda14069
Ben Kotze <befkotze@gmail.com>
2020-12-03 13:55:31 +0200

    Inform user of email success

--- src/components/ContactUsPage.tsx
+++ src/components/ContactUsPage.tsx
@@ -6,6 +6,7 @@ import 'firebase/auth';
 import 'firebase/analytics';
 import 'firebase/firestore';
 import { useForm } from 'react-hook-form';
+import { Alert, Button, Modal } from 'react-bootstrap';
 
 declare global {
   interface Window {
@@ -14,26 +15,24 @@ declare global {
 }
 
 const ContactUsPage: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
   const { register, handleSubmit, errors } = useForm();
   const onSubmit = async (data) => {
     const result = await verifyAsync();
     if (result) {
       console.log(data);
+      handleShow();
     }
   };
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
 
   useEffect(() => {
     window.recaptchaVerifier = new app.auth.RecaptchaVerifier('recaptcha-container', {
       size: 'invisible',
     });
   });
-  //   const doVerifyAsync = async () => {
-  //     const result = await verifyAsync();
-  //     setIsVerified(result);
-  //   };
-  //   if (!isVerified) {
-  //     doVerifyAsync();
-  //   }
 
   const tryToVerifyAsync = async (applicationVerifier: app.auth.ApplicationVerifier) => {
     try {
@@ -51,68 +50,86 @@ const ContactUsPage: React.FC = () => {
   };
 
   return (
-    <Container>
-      <div className="mt-3 mb-3">
-        <h1>Contact Us</h1>
-        You can contact us about anything via email at&nbsp;
-        <a id="emaillink" href="mailto:contact@streetepistemology.com">
-          contact@streetepistemology.com
-        </a>
-        &nbsp;or by filling out the form below:
-        <hr />
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="form-group">
-            <label htmlFor="input-email">Last name *</label>
-            <input
-              type="email"
-              className="form-control"
-              name="email"
-              id="input-email"
-              placeholder="Enter email"
-              ref={register({ pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i, required: true })}
-            />
-            {errors.email && (
-              <small id="email" className="form-text text-danger">
-                Please enter a valid email address.
-              </small>
-            )}
-          </div>
-          <div className="form-group">
-            <label htmlFor="input-subject">Last name *</label>
-            <input
-              className="form-control"
-              name="subject"
-              id="input-subject"
-              placeholder="Enter subject"
-              ref={register({ required: true, minLength: 5, maxLength: 100 })}
-            />
-            {errors.subject && (
-              <small id="subject" className="form-text text-danger">
-                Please enter a subject that has between 5 and 100 characters.
-              </small>
-            )}
-          </div>
-          <div className="form-group">
-            <label htmlFor="input-message">Last name *</label>
-            <textarea
-              className="form-control"
-              name="message"
-              id="input-message"
-              rows={5}
-              placeholder="Enter message"
-              ref={register({ required: true, minLength: 50 })}
-            />
-            {errors.message && (
-              <small id="message" className="form-text text-danger">
-                Please enter a message that is at least 50 characters long.
-              </small>
-            )}
-          </div>
-          <input id="button-submit" type="submit" className="btn btn-primary" />
-        </form>
-        <div id="recaptcha-container" />
-      </div>
-    </Container>
+    <>
+      <Container>
+        <div className="mt-3 mb-3">
+          <h1>Contact Us</h1>
+          You can contact us about anything via email at&nbsp;
+          <a id="emaillink" href="mailto:contact@streetepistemology.com">
+            contact@streetepistemology.com
+          </a>
+          &nbsp;or by filling out the form below:
+          <hr />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-group">
+              <label htmlFor="input-email">Last name *</label>
+              <input
+                type="email"
+                className="form-control"
+                name="email"
+                id="input-email"
+                placeholder="Enter email"
+                ref={register({ pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/i, required: true })}
+              />
+              {errors.email && (
+                <small id="email" className="form-text text-danger">
+                  Please enter a valid email address.
+                </small>
+              )}
+            </div>
+            <div className="form-group">
+              <label htmlFor="input-subject">Last name *</label>
+              <input
+                className="form-control"
+                name="subject"
+                id="input-subject"
+                placeholder="Enter subject"
+                ref={register({ required: true, minLength: 5, maxLength: 100 })}
+              />
+              {errors.subject && (
+                <small id="subject" className="form-text text-danger">
+                  Please enter a subject that has between 5 and 100 characters.
+                </small>
+              )}
+            </div>
+            <div className="form-group">
+              <label htmlFor="input-message">Last name *</label>
+              <textarea
+                className="form-control"
+                name="message"
+                id="input-message"
+                rows={5}
+                placeholder="Enter message"
+                ref={register({ required: true, minLength: 50 })}
+              />
+              {errors.message && (
+                <small id="message" className="form-text text-danger">
+                  Please enter a message that is at least 50 characters long.
+                </small>
+              )}
+            </div>
+            <input id="button-submit" type="submit" className="btn btn-primary" />
+          </form>
+          <div id="recaptcha-container" />
+        </div>
+      </Container>
+
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Thanks for reaching out!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Alert variant="success">
+            Your email message was sent succesfully. You can expect a reply within a couple of days.
+          </Alert>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
