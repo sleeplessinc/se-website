a661a050854a374c9419bacde79561f72562f5d3
Ben Kotze <befkotze@gmail.com>
2020-12-10 22:34:02 +0200

    Display blog metadata

--- src/components/BlogPage.tsx
+++ src/components/BlogPage.tsx
@@ -2,22 +2,27 @@ import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/esm/Container';
 import { FirebaseContext } from '../firebase';
 import parse from 'html-react-parser';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import useStateWithLocalStorage from '../utils/storage';
 import PageNotFound from './PageNotFound';
-import { Button, Spinner } from 'react-bootstrap';
+import { Button, Col, Badge, Row, Spinner } from 'react-bootstrap';
 import { UserContext } from './UserProvider';
+import Blog from '../models/Blog';
+import { formatDistance } from 'date-fns';
+import * as config from '../config.json';
 
 const BlogPage: React.FC = () => {
   const firebaseContext = React.useContext(FirebaseContext);
   const userContext = React.useContext(UserContext);
   const { id } = useParams();
   const path = `blog/${id}`;
+  const [details, setDetails] = useState<Blog | undefined>(undefined);
   const [content, setContent] = useStateWithLocalStorage(path);
   const [notFound, setnotFound] = useState(false);
   const [isLoading, setIsLoading] = useState(content !== '');
+
   useEffect(() => {
-    firebaseContext?.subscribeToPage(
+    return firebaseContext?.subscribeToPage(
       path,
       (results) => {
         if (!results || results === '') {
@@ -34,10 +39,14 @@ const BlogPage: React.FC = () => {
     );
   }, [firebaseContext]);
 
+  useEffect(() => {
+    return firebaseContext?.subscribeToPageDetails(path, setDetails, console.log);
+  }, [firebaseContext]);
+
   return notFound ? (
     <PageNotFound />
   ) : (
-    <Container>
+    <Container className="blog">
       {isLoading ? (
         <div className="text-center">
           <Spinner className="m-5" animation="border" role="status" variant="primary">
@@ -45,10 +54,39 @@ const BlogPage: React.FC = () => {
           </Spinner>
         </div>
       ) : (
-        <div className="blog">
-          {userContext?.isAdmin ? <Button className="position-absolute m-2">Edit Page</Button> : null}
-          {parse(content)}
-        </div>
+        <>
+          {userContext?.isAdmin ? (
+            <Row>
+              <Col />
+              <Col sm="auto" className="mt-2">
+                <Link to={`/${path}/edit`}>
+                  <Button className="m-2">Edit Page</Button>
+                </Link>
+              </Col>
+            </Row>
+          ) : null}
+          {details && (
+            <Row>
+              <Col>
+                <img
+                  src={config.googleCloudBaseUrl + config.bannerFolder + path + '.webp'}
+                  width="100%"
+                  height="auto"
+                />
+                <h1>{details?.title}</h1>
+                by {details?.author}
+                <br />
+                <Badge variant="primary">{details?.category}</Badge>
+                <small>
+                  <p>{'Published ' + formatDistance(new Date(details?.published), new Date()) + ' ago'}</p>
+                </small>
+              </Col>
+            </Row>
+          )}
+          <Row>
+            <Col>{parse(content)}</Col>
+          </Row>
+        </>
       )}
     </Container>
   );
