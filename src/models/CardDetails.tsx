e4f70e45077ccd6b13e467f984d0bcd4e63731a2
Ben Kotze <befkotze@gmail.com>
2020-12-11 10:40:20 +0200

    Changed CardDetails to class

--- src/models/CardDetails.tsx
+++ src/models/CardDetails.tsx
@@ -1,6 +1,13 @@
-export default interface CardDetails {
+export default class CardDetails {
   title: string;
   description: string;
   logo: string;
   url: string;
+
+  constructor() {
+    this.title = '';
+    this.description = '';
+    this.logo = '';
+    this.url = '';
+  }
 }
