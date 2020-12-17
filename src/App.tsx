9eca90d69ecee981bb31545816867778534b82f8
Ben Kotze <befkotze@gmail.com>
2020-12-17 16:59:57 +0200

    Wired in announcement component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Navigation from './components/Navigation';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Navigation from './components/Navigation';
 import MainPage from './components/MainPage';
 import BlogListPage from './components/BlogListPage';
 import ContentPage from './components/ContentPage';
@@ -10,12 +10,14 @@ import Footer from './components/Footer';
 import ContactUsPage from './components/ContactUsPage';
 import Login from './components/Login';
 import EditPage from './components/EditPage';
+import Announcement from './components/Announcement';
 
 const App: React.FC = () => {
   return (
     <Router>
       <div className="App">
         <Navigation />
+        <Announcement />
         <Switch>
           <Route
             path="/:collection/:id/edit"
