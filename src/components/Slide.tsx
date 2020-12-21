906c9302897d92dd95bc5447359f30b1b958ba90
Ben Kotze <befkotze@gmail.com>
2020-12-21 18:08:46 +0200

    Expand slide customization

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -3,25 +3,17 @@ import { Container, Row } from 'react-bootstrap';
 
 export interface SlideProps {
   background: string;
-  title: string;
-  description: string;
+  children?: React.ReactNode | null;
 }
 
-const Slide: React.FC<SlideProps> = ({ background, title, description }: SlideProps) => {
+const Slide: React.FC<SlideProps> = ({ background, children }: SlideProps) => {
   return (
     <Container
       fluid
       className="d-flex align-items-center carousel-slide text-light text-center"
       style={{ backgroundImage: 'url(' + background + ')' }}
     >
-      <Container>
-        <Row>
-          <div className="bg-masked-dark p-2 rounded-xl">
-            <h1 className="montserrat">{title}</h1>
-            <h4>{description}</h4>
-          </div>
-        </Row>
-      </Container>
+      <Container className="bg-masked-dark p-2 rounded-xl">{children}</Container>
     </Container>
   );
 };
