2cb5b62f86891016725b2f8bc7855b0520faf6dd
Ben Kotze <befkotze@gmail.com>
2020-12-21 13:58:35 +0200

    URL string helper for backgrounds

---
+++ src/utils/constants.tsx
@@ -0,0 +1,30 @@
+export const URL_BG_VIDEO_CAMERA =
+  'https://storage.googleapis.com/se-website-fe4a4.appspot.com/images/backgrounds/bg_video_camera.webp';
+export const URL_BG_BOOK =
+  'https://storage.googleapis.com/se-website-fe4a4.appspot.com/images/backgrounds/bg_book.webp';
+export const URL_BG_BUILDINGS_AERIAL_VIEW =
+  'https://storage.googleapis.com/se-website-fe4a4.appspot.com/images/backgrounds/bg_buildings_aerial_view.webp';
+export const URL_BG_COMMUNITY =
+  'https://storage.googleapis.com/se-website-fe4a4.appspot.com/images/backgrounds/bg_community.webp';
+export const URL_BG_FILMING =
+  'https://storage.googleapis.com/se-website-fe4a4.appspot.com/images/backgrounds/bg_filming.webp';
+export const URL_BG_GOPRO =
+  'https://storage.googleapis.com/se-website-fe4a4.appspot.com/images/backgrounds/bg_gopro.webp';
+export const URL_BG_HAND_SHAKE =
+  'https://storage.googleapis.com/se-website-fe4a4.appspot.com/images/backgrounds/bg_hand_shake.webp';
+export const URL_BG_HANDS_RAISED =
+  'https://storage.googleapis.com/se-website-fe4a4.appspot.com/images/backgrounds/bg_hands_raised.webp';
+export const URL_BG_LIGHT_BULB =
+  'https://storage.googleapis.com/se-website-fe4a4.appspot.com/images/backgrounds/bg_light_bulb.webp';
+export const URL_BG_PEDESTRIAN_CROSSING =
+  'https://storage.googleapis.com/se-website-fe4a4.appspot.com/images/backgrounds/bg_pedestrian_crossing.webp';
+export const URL_BG_POCKET_WATCH =
+  'https://storage.googleapis.com/se-website-fe4a4.appspot.com/images/backgrounds/bg_pocket_watch.webp';
+export const URL_BG_QUESTION_MARK =
+  'https://storage.googleapis.com/se-website-fe4a4.appspot.com/images/backgrounds/bg_question_mark.webp';
+export const URL_BG_TAKING_SURVEY =
+  'https://storage.googleapis.com/se-website-fe4a4.appspot.com/images/backgrounds/bg_taking_survey.webp';
+export const URL_BG_THINKING =
+  'https://storage.googleapis.com/se-website-fe4a4.appspot.com/images/backgrounds/bg_thinking.webp';
+export const URL_BG_TOOLS =
+  'https://storage.googleapis.com/se-website-fe4a4.appspot.com/images/backgrounds/bg_tools.webp';
