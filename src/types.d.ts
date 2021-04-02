4156924631a2f7af2b4fdc273601ff7221789dc5
Ben Kotze <befkotze@gmail.com>
2021-04-02 11:49:46 +0200

    Theme provider

---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+export interface ITheme {
+  primary: string;
+  secondary: string;
+  success: string;
+  info: string;
+  warning: string;
+  danger: string;
+  light: string;
+  dark: string;
+}
